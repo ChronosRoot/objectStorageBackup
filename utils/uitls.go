8354a8ac82a934b6051648baaa01438705bc4012
Chronos-T <Chronos.Song@gmail.com>
2022-05-22 02:38:35 +0800

    Fix Bug

--- utils/uitls.go
+++ utils/uitls.go
@@ -10,10 +10,10 @@ import (
 func UpLoadFileKey(localFilePath string) string {
 	systemHostname, _ := os.Hostname()
 	FileNameAll := path.Base(localFilePath)
-	FileExt := path.Ext(FileNameAll)
-	FileName := strings.TrimSuffix(FileNameAll, FileExt)
+	FileName := strings.SplitN(FileNameAll, ".", 2)[0]
+	FileExt := strings.SplitN(FileNameAll, ".", 2)[1]
 	Hostname := strings.Split(systemHostname, ".")[0]
 	UploadDate := time.Now().Format("2006-01-02")
-	UploadFileName := FileName + "-" + UploadDate + FileExt
+	UploadFileName := FileName + "-" + UploadDate + "." + FileExt
 	return strings.Join([]string{Hostname, UploadFileName}, "/")
 }
